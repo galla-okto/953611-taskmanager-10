1ab46462e248d5175d71e5188700835eab21d21e
galla-okto <galla.okto1@gmail.com>
2019-12-03 21:53:17 +0200

    Добавляет ;

--- src/components/filter.js
+++ src/components/filter.js
@@ -18,9 +18,9 @@ const createFilterMarkup = (filter, isChecked) => {
 export const createFilterTemplate = (filters) => {
   const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
 
-    return (
-      `<section class="main__filter filter container">
+  return (
+    `<section class="main__filter filter container">
         ${filtersMarkup}
       </section>`
-    );
-  };
+  );
+};
